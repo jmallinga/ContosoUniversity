c1a2a4c1b889ccb2ad57a9bc91f39cd07846d0a6
jmallinga <jmallinga@gmail.com>
2015-04-20 10:09:30 -0400

    Fixed Web.config file

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -15,29 +15,21 @@ DropCreateDatabaseIfModelChanges<SchoolContext>    {
             var students = new List<Student>
             {
             new
-Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Pa
-rse("2005-09-01")},
+Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
             new
-Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Par
-se("2002-09-01")},
+Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
             new
-Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse(
-"2003-09-01")},
+Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
             new
-Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Par
-se("2002-09-01")}, 
+Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")}, 
             new
-Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-
-09-01")},
+Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
             new
-Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse
-("2001-09-01")},
+Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
             new
-Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse(
-"2003-09-01")},
+Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
             new
-Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse
-("2005-09-01")}
+Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
             };
             students.ForEach(s => context.Students.Add(s));
             context.SaveChanges();
@@ -67,8 +59,9 @@ Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse
             new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
             new Enrollment{StudentID=6,CourseID=1045},
             new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
-            };            enrollments.ForEach(s => context.Enrollments.Add(s));
-            context.SaveChanges();            }
-        }
+            };            
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();                
+        }              
     }
 }
