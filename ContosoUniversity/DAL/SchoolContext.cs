656ecbb88947c4f58b754a686a91abc4e1f0f84b
jmallinga <jmallinga@gmail.com>
2015-04-20 10:13:41 -0400

    Resolved using statement issues

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ContosoUniversity.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
